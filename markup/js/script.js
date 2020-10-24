c54c3bf86bd1d2f90bebb96e65291e15a8d37f4d
Irena Ornatskaya <ornatskayairena@gmail.com>
2020-10-24 23:36:03 +0300

    Timeline added

--- markup/js/script.js
+++ markup/js/script.js
@@ -8,3 +8,25 @@ $(document).ready(function () {
 		$('.wrapper').toggleClass('menu-open');
 	});
 });
+
+$(function(){
+	$().timelinr({
+		orientation: 'horizontal',
+		containerDiv: '#timeline',
+		datesDiv: '#dates',
+		datesSelectedClass: 'selected',
+		datesSpeed: 'normal',
+		issuesDiv : '#issues',
+		issuesSelectedClass: 'selected',
+		issuesSpeed: 'fast',
+		issuesTransparency: 0.2,
+		issuesTransparencySpeed: 500,
+		prevButton: '#prev',
+		nextButton: '#next',
+		arrowKeys: 'false',
+		startAt: 1,
+		autoPlay: 'false',
+		autoPlayDirection: 'forward',
+		autoPlayPause: 2000
+	});
+});
